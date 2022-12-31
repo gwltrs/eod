82d216497d57d2d869d4e348c05d17450446c557
gwltrs <waltersge1@gmail.com>
2022-12-30 21:29:25 -0600

    Indicator: passing

--- test/Indicator.purs
+++ test/Indicator.purs
@@ -24,25 +24,25 @@ indicatorTests = suite "Indicator" do
   test "lastPrice, shiftLeft" do
     Assert.equal (Just 10.0) (indicate' lastPrice noMoves1to10)
     Assert.equal (Just 10.0) (indicate' (shiftLeft 0 lastPrice) noMoves1to10)
-    -- Assert.equal (Just 9.0) (indicate' (shiftLeft 1 lastPrice) noMoves1to10)
-    -- Assert.equal (Just 8.0) (indicate' (shiftLeft 2 lastPrice) noMoves1to10)
-    -- Assert.equal (Just 2.0) (indicate' (shiftLeft 8 lastPrice) noMoves1to10)
-    -- Assert.equal (Just 1.0) (indicate' (shiftLeft 9 lastPrice) noMoves1to10)
-    -- Assert.equal Nothing (indicate' (shiftLeft 10 lastPrice) noMoves1to10)
-  -- test "Functor instance" do
-  --   Assert.equal 
-  --     (Just 48.0) 
-  --     (indicate' (sma 3 # shiftLeft 1 <#> (_ * 2.0)) noMoves1to10)
-  --   Assert.equal 
-  --     Nothing 
-  --     (indicate' (sma 3 # shiftLeft 9 <#> (_ * 2.0)) noMoves1to10)
-  -- test "Applicative instance" do
-  --   Assert.equal 
-  --     (Just 486.0) 
-  --     (indicate' ((*) <$> sma 3 <*> (sma 3 # shiftLeft 3)) noMoves1to10)
-  --   Assert.equal 
-  --     Nothing 
-  --     (indicate' ((*) <$> sma 3 <*> (sma 3 # shiftLeft 10)) noMoves1to10)
+    Assert.equal (Just 9.0) (indicate' (shiftLeft 1 lastPrice) noMoves1to10)
+    Assert.equal (Just 8.0) (indicate' (shiftLeft 2 lastPrice) noMoves1to10)
+    Assert.equal (Just 2.0) (indicate' (shiftLeft 8 lastPrice) noMoves1to10)
+    Assert.equal (Just 1.0) (indicate' (shiftLeft 9 lastPrice) noMoves1to10)
+    Assert.equal Nothing (indicate' (shiftLeft 10 lastPrice) noMoves1to10)
+  test "Functor instance" do
+    Assert.equal 
+      (Just 16.0) 
+      (indicate' (sma 3 # shiftLeft 1 <#> (_ * 2.0)) noMoves1to10)
+    Assert.equal 
+      Nothing 
+      (indicate' (sma 3 # shiftLeft 9 <#> (_ * 2.0)) noMoves1to10)
+  test "Applicative instance" do
+    Assert.equal 
+      (Just 54.0) 
+      (indicate' ((*) <$> sma 3 <*> (sma 3 # shiftLeft 3)) noMoves1to10)
+    Assert.equal 
+      Nothing 
+      (indicate' ((*) <$> sma 3 <*> (sma 3 # shiftLeft 10)) noMoves1to10)
 
 noMoves1to10 :: Array LiveDay
 noMoves1to10 = noMove <$> toNumber <$> range 1 10

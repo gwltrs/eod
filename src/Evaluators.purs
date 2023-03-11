be7e1905301c7960d01d6ad90972009c9f9c3628
gwltrs <waltersge1@gmail.com>
2023-03-11 15:07:05 -0600

    expectancy, standardDeviation, systemQuality

--- src/Evaluators.purs
+++ src/Evaluators.purs
@@ -5,28 +5,28 @@ import Prelude
 import Data.Array (filter, length)
 import Data.Int (toNumber)
 import Data.Foldable (class Foldable, foldr)
+import Data.Ord (abs)
+import Data.Number (pow, sqrt)
 import Utils (undefined)
 
 expectancy :: Array Number -> Number
-expectancy rMultiples = 
-  let 
-    winners = filter (_ > 0.0) rMultiples
-    losers = filter (_ < 0.0) rMultiples
-    fLen arr = toNumber $ length arr
-    avg arr = (foldr (+) 0.0 arr) / (fLen arr)
-    calc [] [] = -2.0
-    calc [] l = -2.0
-    calc w [] = -2.0
-    calc w l = 
-      let 
-        avgProfit = avg w
-        avgLoss = avg l
-        probWin = (fLen w) / (fLen rMultiples)
-        probLoss = (fLen l) / (fLen rMultiples)
-      in
-        (avgProfit * probWin) - (avgLoss * probLoss)
-  in
-    calc winners losers
+expectancy rMultiples = (foldr (+) 0.0 rMultiples) / (toNumber $ length rMultiples)
 
---systemQuality :: Number -> Number -> Number
---systemQuality = undefined
+standardDeviation :: Array Number -> Number
+standardDeviation [] = 0.0
+standardDeviation ns = 
+  let
+    mean = foldr (+) 0.0 ns  / (toNumber $ length ns) 
+    diffs = ns <#> (\x -> pow (x - mean) 2.0) # foldr (+) 0.0
+    n = (toNumber $ length ns)
+  in 
+    sqrt (diffs / n)
+
+systemQuality :: Array Number -> Number
+systemQuality rMultiples = 
+  let 
+    e = expectancy rMultiples
+    std = standardDeviation rMultiples
+    sqrtN = sqrt $ toNumber $ length rMultiples
+  in
+    (e / std) * sqrtN
--- test/Evaluators.purs
+++ test/Evaluators.purs
@@ -4,16 +4,28 @@ import Prelude
 import Control.Monad.Free (Free)
 import Test.Unit (suite, test, TestF)
 import Test.Unit.Assert as Assert
+import Test.Unit (Test)
 import Type.YMD (ymd)
 import Forceable (frc)
-import Evaluators (expectancy)
+import Evaluators (expectancy, systemQuality, standardDeviation)
+import Data.Int (toNumber)
+import Data.Number.Approximate (eqAbsolute, Tolerance(..))
 
 evaluatorsTests :: Free TestF Unit
 evaluatorsTests = suite "Evaluators" do
   test "expectancy" do
-   Assert.equal 0.49 (expectancy table2_6)
+    withinHundredth 0.72 (expectancy table3_6)
+  test "systemQuality" do
+    withinHundredth 0.75 (systemQuality table3_6)
+  test "standardDeviation" do
+    withinHundredth 4.81 (standardDeviation table3_6)
 
-table2_6 :: Array Number
-table2_6 = [
-  6.58, 7.7, 1.38, 2.23, 7.53, 2.2, -(1.09), -(1.4), -(0.85), 1.71, -(2.58), 
-  -(0.11), -(0.1), -(1.02), -(0.81), 0.26, -(1.4), -(1.0), -(1.2), -(3.56), -(2.8), -(1.0)]
+table3_6 :: Array Number
+table3_6 = [-1,-1,-1,-1,-1,-1,10,-1,-5,-5,-1,-1,-1,-1,10,-1,-5,-1,10,-1,10,-1,-1,10,-1]
+  <#> toNumber
+
+withinHundredth :: Number -> Number -> Test
+withinHundredth expected actual =
+  Assert.assert 
+    ("Error: expected " <> show expected <> ", got " <> show actual)
+    (eqAbsolute (Tolerance 0.01) expected actual)

4b6e7032ef7f996f4aea61a9669a072de49abf6a
gwltrs <waltersge1@gmail.com>
2023-03-20 22:53:34 -0500

    maxPreviousLow tests 2/?

--- test/Evaluators.purs
+++ test/Evaluators.purs
@@ -15,6 +15,7 @@ import Type.Purchase (Purchase, mkPurchase)
 import Forceable (($!), frc)
 import Type.Alias (RMultiple)
 import NestedApplicative ((<<*>>))
+import Partial.Unsafe (unsafeCrashWith)
 
 evaluatorsTests :: Free TestF Unit
 evaluatorsTests = suite "Evaluators" do
@@ -26,12 +27,20 @@ evaluatorsTests = suite "Evaluators" do
     Assert.equal (Just $ fourPrice 3.0)   (evaluate' (at 1) $ [doji, fourPrice 3.0])
     Assert.equal Nothing                  (evaluate' (at 2) $ [doji, fourPrice 3.0])
   test "maxPreviousLow" do
-    Assert.equal 0.0 (maxPreviousLow' (frc $ mkPurchase 2.0 1.0) [])
+    withinHundredth 0.0 (maxPreviousLow' (mkP 2.0 1.0) [])
+    withinHundredth 1.0 (maxPreviousLow' (mkP 2.0 1.0) [fourPrice 3.0])
+    withinHundredth 0.0 (maxPreviousLow' (mkP 2.0 1.0) [fourPrice 2.0])
+    withinHundredth (-1.0) (maxPreviousLow' (mkP 2.0 1.0) [fourPrice 1.0])
+    withinHundredth (-2.0) (maxPreviousLow' (mkP 2.0 1.0) [fourPrice 0.0])
 
 maxPreviousLow' :: Purchase -> Array Day -> RMultiple
--- evaluate' :: forall a. Evaluator a -> Array Day -> Maybe a
--- maxPreviousLow :: Evaluator (Purchase -> RMultiple)
-maxPreviousLow' p a = frc $ ((evaluate' maxPreviousLow a) <*> (Just p))
+maxPreviousLow' p a = 
+  case (evaluate' maxPreviousLow a) <*> (Just p) of
+    (Just r) -> r
+    Nothing -> unsafeCrashWith "maxPreviousLow' returned Nothing"
+
+mkP :: Number -> Number -> Purchase
+mkP buy stop = frc $ mkPurchase buy stop
 
 withinHundredth :: Number -> Number -> Test
 withinHundredth expected actual =
